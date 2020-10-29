888fa75de96a39a0c82f7ebd52cb461cc1a7996c
Diógenes Virgilio <diogenesvirgilio@ymail.com>
2020-10-29 09:03:53 -0300

    banco de dados

---
+++ twitter_clone.sql
@@ -0,0 +1,147 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 29-Out-2020 às 12:56
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `twitter_clone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tweets`
+--
+
+CREATE TABLE `tweets` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `tweet` varchar(140) COLLATE utf8_unicode_ci NOT NULL,
+  `data` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `tweets`
+--
+
+INSERT INTO `tweets` (`id`, `id_usuario`, `tweet`, `data`) VALUES
+(2, 3, 'teste de novo', '2020-08-17 14:20:45'),
+(4, 1, 'Jogar bola hoje ?', '2020-08-17 16:47:16'),
+(5, 1, 'Hoje não !', '2020-08-17 16:47:44'),
+(10, 2, 'Fazer mais codigos', '2020-08-25 13:21:44'),
+(12, 3, 'Teste de exclusao', '2020-08-26 17:02:00'),
+(14, 1, 'Segunda braba', '2020-09-14 14:39:08');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(32) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'Gabriel Batistuta', 'gabrielbatistuta@hotmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
+(2, 'Pele café', 'pelemarreta@marreta', 'e10adc3949ba59abbe56e057f20f883e'),
+(3, 'Arlindo Pinto Pereira', 'arlindopinto@hotmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
+(4, 'Sheldon Cooper', 'sheldon@bigbang.com', 'e10adc3949ba59abbe56e057f20f883e'),
+(5, 'Rafael Leão', 'rafaleao@hotmail.com', 'e10adc3949ba59abbe56e057f20f883e');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios_seguidores`
+--
+
+CREATE TABLE `usuarios_seguidores` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_usuario_seguindo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `usuarios_seguidores`
+--
+
+INSERT INTO `usuarios_seguidores` (`id`, `id_usuario`, `id_usuario_seguindo`) VALUES
+(8, 2, 4),
+(13, 2, 5),
+(14, 2, 1),
+(15, 1, 2),
+(16, 1, 4),
+(17, 1, 3),
+(18, 1, 5),
+(19, 3, 1);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tweets`
+--
+ALTER TABLE `tweets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuarios_seguidores`
+--
+ALTER TABLE `usuarios_seguidores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tweets`
+--
+ALTER TABLE `tweets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios_seguidores`
+--
+ALTER TABLE `usuarios_seguidores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
